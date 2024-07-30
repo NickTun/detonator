7ce60479efd191bac3734e58b1c2cbeb2c22bced
nicktun <nikolaevsky.igor.a@gmail.com>
2024-07-30 12:31:44 -0400

    port changes

--- __init__.py
+++ __init__.py
@@ -8,34 +8,35 @@ load_dotenv()
 RCON_PASS=os.getenv('RCON_PASS')
 SERVER_IP=os.getenv('SERVER_IP')
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
+with Client(SERVER_IP, 8270, passwd=RCON_PASS) as client:
+    print(">> RCON CONNECTED")
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+    def create_app(test_config=None):
+        # create and configure the app
+        app = Flask(__name__, instance_relative_config=True)
+        app.config.from_mapping(
+            SECRET_KEY='dev',
+            DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        )
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+        if test_config is None:
+            # load the instance config, if it exists, when not testing
+            app.config.from_pyfile('config.py', silent=True)
+        else:
+            # load the test config if passed in
+            app.config.from_mapping(test_config)
 
-    # a simple page that says hello
-    @app.route('/boom')
-    def boom():
-        print('>> REQUEST RECIEVED')
-        with Client(SERVER_IP, 8101, passwd=RCON_PASS) as client:
-            print(">> RCON CONNECTED")
+        # ensure the instance folder exists
+        try:
+            os.makedirs(app.instance_path)
+        except OSError:
+            pass
+
+        # a simple page that says hello
+        @app.route('/boom')
+        def boom():
+            print('>> REQUEST RECIEVED')
             client.run("kill @a")
-        return 'Allahu akbar'
+            return 'Allahu akbar'
 
-    return app
+        return app
