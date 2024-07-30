ef17a0c71a985266cbc9ab75e019dd773b7488ea
nicktun <nikolaevsky.igor.a@gmail.com>
2024-07-30 11:56:05 -0400

    moved to rcon

--- __init__.py
+++ __init__.py
@@ -1,40 +1,42 @@
 import os
-
+from dotenv import load_dotenv
 from flask import Flask
+from rcon.source import Client
 
-from mcpi.minecraft import Minecraft
+load_dotenv()
 
-mc = Minecraft.create("server.altair.one", 8270)
-mc.postToChat(">> DETONATOR CONNECTED")
-print(mc.player.getTilePos())
+RCON_PASS=os.getenv('RCON_PASS')
+SERVER_IP=os.getenv('SERVER_IP')
 
+with Client(SERVER_IP, 8101, passwd=RCON_PASS) as client:
+    print(">> RCON CONNECTED")
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
+    def create_app(test_config=None):
+        # create and configure the app
+        app = Flask(__name__, instance_relative_config=True)
+        app.config.from_mapping(
+            SECRET_KEY='dev',
+            DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        )
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+        if test_config is None:
+            # load the instance config, if it exists, when not testing
+            app.config.from_pyfile('config.py', silent=True)
+        else:
+            # load the test config if passed in
+            app.config.from_mapping(test_config)
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+        # ensure the instance folder exists
+        try:
+            os.makedirs(app.instance_path)
+        except OSError:
+            pass
 
-    # a simple page that says hello
-    @app.route('/boom')
-    def boom():
-        print('>> REQUEST RECIEVED')
-        mc.postToChat("Ur to be allah u akhbared")
-        return 'Allahu akbar'
+        # a simple page that says hello
+        @app.route('/boom')
+        def boom():
+            print('>> REQUEST RECIEVED')
+            client.run("kill @a")
+            return 'Allahu akbar'
 
-    return app
+        return app
