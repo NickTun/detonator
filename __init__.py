c405f5e3cdd13d69aca540ea1ed099d075e0fddc
nicktun <nikolaevsky.igor.a@gmail.com>
2024-07-31 08:00:06 -0400

    shooting mechanism implemented

--- __init__.py
+++ __init__.py
@@ -1,12 +1,13 @@
-import os
+import os, math, time, random
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, request
 from rcon.source import Client
 
 load_dotenv()
 
 RCON_PASS=os.getenv('RCON_PASS')
 SERVER_IP=os.getenv('SERVER_IP')
+SERVER_PORT=os.getenv('SERVER_PORT')
 
 def create_app(test_config=None):
     # create and configure the app
@@ -33,9 +34,33 @@ def create_app(test_config=None):
     @app.route('/boom')
     def boom():
         print('>> REQUEST RECIEVED')
-        with Client(SERVER_IP, 8270, passwd=RCON_PASS) as client:
+        with Client(SERVER_IP, int(SERVER_PORT), passwd=RCON_PASS) as client:
             print(">> RCON CONNECTED")
-            client.run("execute as @e at @s run summon minecraft:tnt")
+            client.run("execute as @e at @s run summon tnt")
+        return 'Allahu akbar'
+    
+    @app.route('/<player_name>/shoot')
+    def shoot(player_name):
+        print('>> REQUEST RECIEVED')
+        with Client(SERVER_IP, int(SERVER_PORT), passwd=RCON_PASS) as client:
+            print(">> RCON CONNECTED")
+
+            client.run('summon marker 0 0 0 {Tags:["position"]}')
+            client.run('data modify entity @e[tag=position,limit=1] Rotation set from entity '+player_name+' Rotation')
+            client.run('execute as @e[tag=position,limit=1] at @s run summon marker ^ ^ ^9 {Tags:["direction"]}')
+
+            client.run('execute as '+player_name+' at @s run summon arrow ^ ^ ^1 {fuse:5,Tags:["projectile"]}')
+
+            client.run('data modify entity @e[tag=projectile,limit=1] Motion set from entity @e[tag=direction,limit=1] Pos')
+
+            time.sleep(0.5)
+            for i in range(20):
+                client.run('execute as @e[tag=projectile, limit=1] at @s run summon tnt ~'+str(random.randint(-2,2))+' ~'+str(random.randint(-2,2))+' ~'+str(random.randint(-2,2))+' {fuse:'+str(i)+'}')
+                client.run('execute as @e[tag=projectile, limit=1] at @s run summon lightning_bolt ~'+str(random.randint(-5,5))+' ~'+str(random.randint(-5,5))+' ~'+str(random.randint(-5,5)))
+                client.run('execute as @e[tag=projectile, limit=1] at @s run summon falling_block ~'+str(random.randint(-2,2))+' ~'+str(random.randint(-2,2))+' ~'+str(random.randint(-2,2))+' {BlockState:{Name:"minecraft:fire"},Time:1}')
+            client.run('kill @e[tag=projectile]')
+            client.run('kill @e[tag=position]')
+            client.run('kill @e[tag=direction]')
         return 'Allahu akbar'
 
     return app
